ff7d941b8145d1660b2425a5eec3178df7deb4b8
ace <673189914@qq.com>
2021-02-04 16:40:45 +0800

    v2

--- app/src/main/java/com/example/campusinformationplatform/Release_Page.java
+++ app/src/main/java/com/example/campusinformationplatform/Release_Page.java
@@ -1,16 +1,23 @@
 package com.example.campusinformationplatform;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.ContactsContract;
 import android.provider.MediaStore;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,8 +26,13 @@ import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,7 +41,15 @@ public class Release_Page extends AppCompatActivity {
     //private int[] images = {R.drawable.add_img2,};
 
     private List<Bitmap> BitmapList = new ArrayList<Bitmap>();
+    private List<Bitmap> SendingImgList = new ArrayList<Bitmap>();
+
     private GridView gridImgView;
+
+    private int gridImgViewSize=300;
+
+    int clickNum=0;
+
+    private AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,7 +58,7 @@ public class Release_Page extends AppCompatActivity {
 
 
         BitmapList.add(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.add_img2),
-                300,300, true));//类型的转换);
+                gridImgViewSize,gridImgViewSize, true));//类型的转换);
         gridImgView = (GridView) findViewById(R.id.Release_Img_GridView);
         gridImgView.setAdapter(new MyAdapter(this,BitmapList));
 
@@ -46,14 +66,40 @@ public class Release_Page extends AppCompatActivity {
 
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
+                                    final int position, long id) {
                 // TODO Auto-generated method stub
                 Log.d(String.valueOf(position), "onItemClick: ");
 
+                if(position==BitmapList.size()-1){//添加图片
+                    Intent intent = new Intent(Intent.ACTION_PICK, null);
+                    intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                    startActivityForResult(intent, 2);
 
-                Intent intent = new Intent(Intent.ACTION_PICK, null);
-                intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent, 2);
+                }else{
+                    clickNum++;
+                    final Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (clickNum == 1) {//单击
+                                Log.d("", "单击");
+
+                            }else if(clickNum==2){//双击
+                                Log.d("", "双击");
+
+                                showWhetheOrNotToDeleteImg(position);
+
+                            }
+                            else{
+
+                            }
+                            //防止handler引起的内存泄漏
+                            handler.removeCallbacksAndMessages(null);
+                            clickNum = 0;
+                        }
+                    },300);
+                }
+
 
 
             }
@@ -73,19 +119,79 @@ public class Release_Page extends AppCompatActivity {
 
                 Log.e("uri", uri.toString());
                 BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inSampleSize = 32;
+                options.inSampleSize = 2;
                 Bitmap bitmap_Pic = BitmapFactory.decodeFile(getRealFilePath(this, uri), options);
 
+                if(SendingImgList.size()==0)
+                    SendingImgList.add(bitmap_Pic);
+                else
+                    SendingImgList.add(SendingImgList.size()-1,bitmap_Pic);
+
+                System.out.println("bitmap_Pic H:"+String.valueOf( bitmap_Pic.getHeight()));
+                System.out.println("bitmap_Pic W:"+String.valueOf( bitmap_Pic.getWidth()));
+
+                Bitmap ScaleBitmap=getScaleBitmap(bitmap_Pic);
+
+                System.out.println("temp H:"+String.valueOf( ScaleBitmap.getHeight()));
+                System.out.println("temp W:"+String.valueOf( ScaleBitmap.getWidth()));
+
+                Bitmap showPic;
+                if(ScaleBitmap.getHeight()>ScaleBitmap.getWidth()){
+                    //比较高
+                    showPic = Bitmap.createBitmap(ScaleBitmap, 0, ScaleBitmap.getHeight()/2-150, 300, 300);
+
+                }
+                else{
+                    showPic = Bitmap.createBitmap(ScaleBitmap, ScaleBitmap.getWidth()/2-150, 0, 300, 300);
+                }
 
 
-                BitmapList.add(Bitmap.createBitmap(bitmap_Pic,0,0,100,100));
 
+                BitmapList.add(BitmapList.size()-1,showPic);
+                //BitmapList.add(showPic);
 
                 gridImgView.setAdapter(new MyAdapter(this,BitmapList));
+
+
+
 
             }
         }
     }
+
+    private void showWhetheOrNotToDeleteImg(final int position) {
+
+        builder = new AlertDialog.Builder(this).setIcon(R.mipmap.ic_launcher).setTitle("11")
+                .setMessage("是否删除图片？");
+
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                SendingImgList.remove(position-1);
+                BitmapList.remove(position);
+                gridImgView.setAdapter(new MyAdapter(getApplicationContext(),BitmapList));
+
+
+                Toast.makeText(getApplicationContext(), "删除图片成功",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+        }
+        });
+
+
+
+        builder.create().show();
+    }
+
+
 
     //url
     public static String getRealFilePath(final Context context, final Uri uri) {
@@ -114,10 +220,41 @@ public class Release_Page extends AppCompatActivity {
         return data;
     }
 
+    private Bitmap getScaleBitmap(Bitmap bm){
+        int bitmapWidth=bm.getWidth();
+        int bitmapHeight=bm.getHeight();
+        float scaleWidth;
+        float scaleHeight;
+
+        int newWidth;
+        int newHeight;
+
+        float scale=(float)bitmapWidth/(float)bitmapHeight;
+        if(scale>=1){//比较宽,正方形
+           newHeight=gridImgViewSize;
+           newWidth=(int)(newHeight*scale);
+        }
+        else{//比较长
+            newWidth=gridImgViewSize;
+            newHeight=(int)(newWidth/scale);
+        }
+
+        // 计算缩放比例
+        scaleWidth = ((float) newWidth) / bitmapWidth;
+        scaleHeight = ((float) newHeight) / bitmapHeight;
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        // 得到新的图片
+        Bitmap newbm = Bitmap.createBitmap(bm, 0, 0, bitmapWidth, bitmapHeight, matrix,
+                true);
+
+        return newbm;
+    }
+
 
     private class MyAdapter extends BaseAdapter {
         private LayoutInflater layoutInflater;
-        //private int[] images;
 
         private List<Bitmap> BitmapList = new ArrayList<Bitmap>();
 
@@ -146,6 +283,11 @@ public class Release_Page extends AppCompatActivity {
             View v = layoutInflater.inflate(R.layout.gridimageviewitem,null);
 
             ImageView iv = (ImageView) v.findViewById(R.id.image_gridView_item);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(gridImgViewSize,
+                    gridImgViewSize);
+            iv.setLayoutParams(params);
+
             iv.setImageBitmap(BitmapList.get(position));
 
             return v;
